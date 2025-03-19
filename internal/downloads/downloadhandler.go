4290d9ce06570982ba0c5d71013e5fd5897c9116
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2025-03-19 12:05:10 +0330

    chore: move ticker

--- internal/downloads/downloadhandler.go
+++ internal/downloads/downloadhandler.go
@@ -1,10 +1,11 @@
 package downloads
 
 import (
-	"github.com/computer-technology-team/download-manager.git/internal/queues"
+	"github.com/computer-technology-team/download-manager.git/internal/bandwidthlimit"
 	"github.com/computer-technology-team/download-manager.git/internal/state"
 )
 
-func NewDownloadHandler(downloadConfig state.Download, downloadChuncks []state.DownloadChunk, ticker queues.Ticker) DownloadHandler { 
+func NewDownloadHandler(downloadConfig state.Download, downloadChuncks []state.DownloadChunk, ticker bandwidthlimit.Ticker) DownloadHandler {
 	return nil
 }
+
