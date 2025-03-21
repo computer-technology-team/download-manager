76b655b2238080ade8ec58cc630f8b39a76b1c55
Hami <hamiebad79@gmail.com>
2025-03-21 19:09:55 +0330

    small change

--- internal/downloads/downloadhandler.go
+++ internal/downloads/downloadhandler.go
@@ -16,50 +16,47 @@ func NewDownloadHandler(downloadConfig state.Download, downloadChuncks []state.D
 	if _, err := os.Stat(savePath); err == nil {
 		return nil, fmt.Errorf("file already exists at %s", savePath)
 	} else if !os.IsNotExist(err) {
-		// If the error is not due to the file not existing, return it
 		return nil, fmt.Errorf("error checking file at %s: %w", savePath, err)
-	} else {
-		pausedChan := make(chan int, 1)
-		close(pausedChan)
+	}
+	pausedChan := make(chan int, 1)
+	close(pausedChan)
 
-		defDow := defaultDownloader{
-			id:            downloadConfig.ID,
-			queueID:       downloadConfig.QueueID,
-			url:           downloadConfig.Url,
-			savePath:      downloadConfig.SavePath,
-			state:         DownloadState(downloadConfig.State),
-			limiter:       limiter,
-			chunkHandlers: nil,
-			progress:      0,
-			progressRate:  0,
-			size:          0,
-			pausedChan:    nil,
-			ctx:           nil,
-			ctxCancel:     nil,
-			writer:        NewSynchronizedFileWriter(downloadConfig.SavePath),
-			failedChannel: make(chan interface{}),
-			wg:            sync.WaitGroup{},
-		}
-
-		defDow.pausedChan = &pausedChan
-
-		if len(downloadChuncks) == numberOfChuncks {
-			chunkhandlersList := make([]*DownloadChunkHandler, numberOfChuncks)
-
-			for i, chunk := range downloadChuncks {
-
-				handler := NewDownloadChunkHandler(chunk, defDow.pausedChan, defDow.failedChannel, &defDow.wg)
-
-				chunkhandlersList[i] = handler
-			}
-			defDow.chunkHandlers = chunkhandlersList
-		} else if len(downloadChuncks) == 1 && downloadChuncks[0].SinglePart {
-			defDow.chunkHandlers = []*DownloadChunkHandler{
-				NewDownloadChunkHandler(downloadChuncks[0], defDow.pausedChan, defDow.failedChannel, &defDow.wg),
-			}
-		}
-
-		return &defDow, nil
+	defDow := defaultDownloader{
+		id:            downloadConfig.ID,
+		queueID:       downloadConfig.QueueID,
+		url:           downloadConfig.Url,
+		savePath:      downloadConfig.SavePath,
+		state:         DownloadState(downloadConfig.State),
+		limiter:       limiter,
+		chunkHandlers: nil,
+		progress:      0,
+		progressRate:  0,
+		size:          0,
+		pausedChan:    nil,
+		ctx:           nil,
+		ctxCancel:     nil,
+		writer:        NewSynchronizedFileWriter(downloadConfig.SavePath),
+		failedChannel: make(chan interface{}),
+		wg:            sync.WaitGroup{},
 	}
 
+	defDow.pausedChan = &pausedChan
+
+	if len(downloadChuncks) == numberOfChuncks {
+		chunkhandlersList := make([]*DownloadChunkHandler, numberOfChuncks)
+
+		for i, chunk := range downloadChuncks {
+
+			handler := NewDownloadChunkHandler(chunk, defDow.pausedChan, defDow.failedChannel, &defDow.wg)
+
+			chunkhandlersList[i] = handler
+		}
+		defDow.chunkHandlers = chunkhandlersList
+	} else if len(downloadChuncks) == 1 && downloadChuncks[0].SinglePart {
+		defDow.chunkHandlers = []*DownloadChunkHandler{
+			NewDownloadChunkHandler(downloadChuncks[0], defDow.pausedChan, defDow.failedChannel, &defDow.wg),
+		}
+	}
+
+	return &defDow, nil
 }
