9222c8522d829c4ec378519bbf4a87652f8a2dda
Hami <hamiebad79@gmail.com>
2025-03-21 18:41:08 +0330

    throw error when file already exists in savePath

--- internal/downloads/downloadhandler.go
+++ internal/downloads/downloadhandler.go
@@ -1,52 +1,65 @@
 package downloads
 
 import (
+	"fmt"
+	"os"
 	"sync"
 
 	"github.com/computer-technology-team/download-manager.git/internal/bandwidthlimit"
 	"github.com/computer-technology-team/download-manager.git/internal/state"
 )
 
-func NewDownloadHandler(downloadConfig state.Download, downloadChuncks []state.DownloadChunk, limiter *bandwidthlimit.Limiter) DownloadHandler {
-	pausedChan := make(chan int, 1)
-	close(pausedChan)
+func NewDownloadHandler(downloadConfig state.Download, downloadChuncks []state.DownloadChunk, limiter *bandwidthlimit.Limiter) (DownloadHandler, error) {
 
-	defDow := defaultDownloader{
-		id:            downloadConfig.ID,
-		queueID:       downloadConfig.QueueID,
-		url:           downloadConfig.Url,
-		savePath:      downloadConfig.SavePath,
-		state:         DownloadState(downloadConfig.State),
-		limiter:       limiter,
-		chunkHandlers: nil,
-		progress:      0,
-		progressRate:  0,
-		size:          0,
-		pausedChan:    nil,
-		ctx:           nil,
-		ctxCancel:     nil,
-		writer:        NewSynchronizedFileWriter(downloadConfig.SavePath),
-		failedChannel: make(chan interface{}),
-		wg:            sync.WaitGroup{},
+	savePath := downloadConfig.SavePath
+
+	if _, err := os.Stat(savePath); err == nil {
+		return nil, fmt.Errorf("file already exists at %s", savePath)
+	} else if !os.IsNotExist(err) {
+		// If the error is not due to the file not existing, return it
+		return nil, fmt.Errorf("error checking file at %s: %w", savePath, err)
+	} else {
+		pausedChan := make(chan int, 1)
+		close(pausedChan)
+
+		defDow := defaultDownloader{
+			id:            downloadConfig.ID,
+			queueID:       downloadConfig.QueueID,
+			url:           downloadConfig.Url,
+			savePath:      downloadConfig.SavePath,
+			state:         DownloadState(downloadConfig.State),
+			limiter:       limiter,
+			chunkHandlers: nil,
+			progress:      0,
+			progressRate:  0,
+			size:          0,
+			pausedChan:    nil,
+			ctx:           nil,
+			ctxCancel:     nil,
+			writer:        NewSynchronizedFileWriter(downloadConfig.SavePath),
+			failedChannel: make(chan interface{}),
+			wg:            sync.WaitGroup{},
+		}
+
+		defDow.pausedChan = &pausedChan
+
+		if len(downloadChuncks) == numberOfChuncks {
+			chunkhandlersList := make([]*DownloadChunkHandler, numberOfChuncks)
+
+			for i, chunk := range downloadChuncks {
+
+				handler := NewDownloadChunkHandler(chunk, defDow.pausedChan, defDow.failedChannel, &defDow.wg)
+
+				chunkhandlersList[i] = handler
+			}
+			defDow.chunkHandlers = chunkhandlersList
+		} else if len(downloadChuncks) == 1 && downloadChuncks[0].SinglePart {
+			defDow.chunkHandlers = []*DownloadChunkHandler{
+				NewDownloadChunkHandler(downloadChuncks[0], defDow.pausedChan, defDow.failedChannel, &defDow.wg),
+			}
+		}
+
+		return &defDow, nil
 	}
 
-	defDow.pausedChan = &pausedChan
-
-	if len(downloadChuncks) == numberOfChuncks {
-		chunkhandlersList := make([]*DownloadChunkHandler, numberOfChuncks)
-
-		for i, chunk := range downloadChuncks {
-
-			handler := NewDownloadChunkHandler(chunk, defDow.pausedChan, defDow.failedChannel, &defDow.wg)
-
-			chunkhandlersList[i] = handler
-		}
-		defDow.chunkHandlers = chunkhandlersList
-	} else if len(downloadChuncks) == 1 && downloadChuncks[0].SinglePart {
-		defDow.chunkHandlers = []*DownloadChunkHandler{
-			NewDownloadChunkHandler(downloadChuncks[0], defDow.pausedChan, defDow.failedChannel, &defDow.wg),
-		}
-	}
-
-	return &defDow
 }
